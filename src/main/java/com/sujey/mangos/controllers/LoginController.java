92836cdd1ab33b6992dd8ace67c7476ff3ac828e
SujeyCalderon <233291@ids.upchiapas.edu.mx>
2024-03-13 03:10:36 -0600

    Interfaz de inicio de sesion

--- src/main/java/com/sujey/mangos/controllers/LoginController.java
+++ src/main/java/com/sujey/mangos/controllers/LoginController.java
@@ -1,4 +1,18 @@
 package com.sujey.mangos.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+
+    @FXML
+    private TextField contraseñaUsuario;
+
+    @FXML
+    private Button ingresarUsuario;
+
+    @FXML
+    private TextField nombreUsuario;
+
 }
