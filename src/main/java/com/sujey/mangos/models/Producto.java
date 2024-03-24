fb61f4c571279b6a4f6be7b0de06c08236c31209
SujeyCalderon <233291@ids.upchiapas.edu.mx>
2024-03-23 20:59:41 -0600

    Interfaces terminadas

--- src/main/java/com/sujey/mangos/models/Producto.java
+++ src/main/java/com/sujey/mangos/models/Producto.java
@@ -1,4 +1,37 @@
 package com.sujey.mangos.models;
 
 public class Producto {
+    protected String nombre;
+    protected String Fecha;
+    protected String Descripcion;
+
+    public Producto(String nombre, String fecha, String descripcion) {
+        this.nombre = nombre;
+        Fecha = fecha;
+        Descripcion = descripcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFecha() {
+        return Fecha;
+    }
+
+    public void setFecha(String fecha) {
+        Fecha = fecha;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        Descripcion = descripcion;
+    }
 }
