05f2bd4a7579e1dd65cd77c44609cff1e61bbc39
HanniaPaola <233273@ids.upchiapas.edu.mx>
2024-04-01 05:56:56 -0600

    Optimización de guardar los datos

--- src/main/java/com/sujey/mangos/controllers/MedicamentosAgregarController.java
+++ src/main/java/com/sujey/mangos/controllers/MedicamentosAgregarController.java
@@ -1,9 +1,13 @@
 package com.sujey.mangos.controllers;
+import com.sujey.mangos.models.Medicamento;
 
 import com.sujey.mangos.Login;
+import com.sujey.mangos.models.Administracion;
+import com.sujey.mangos.models.Venta;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
@@ -70,11 +74,61 @@ public class MedicamentosAgregarController {
             throw new RuntimeException(e);
         }
     }
-
+    
     @FXML
     void MouseClickGuardar(MouseEvent event) {
+        String nombre = Tes6.getText();
+        String fecha = Tes10.getText();
+        String descripción = Tes9.getText();
+        double cantidad;
+        double precio;
+        try {
+            cantidad = Double.parseDouble(Tes7.getText());
+        } catch (NumberFormatException e) {
+            mostrarError();
+            return;
+        }
+        try {
+            precio = Double.parseDouble(Tes8.getText());
+        } catch (NumberFormatException e) {
+            mostrarError2();
+            return;
+        }
+        Medicamento medicamento = new Medicamento(nombre, fecha, descripción, cantidad, precio);
+        Administracion administracion = Login.getAdmin();
+        administracion.addMedicamento(medicamento);
+
+        Tes6.clear();
+        Tes7.clear();
+        Tes8.clear();
+        Tes9.clear();
+        Tes10.clear();
+
+        mostrarMensajeGuardado();
 
     }
+    private void mostrarMensajeGuardado() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Guardado exitoso");
+        alert.setHeaderText(null);
+        alert.setContentText("Se ha añadido el Medicamento con éxito.");
+        alert.showAndWait();
+    }
+    private void mostrarError() {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("ERROR");
+        alert.setHeaderText(null);
+        alert.setContentText("El precio debe ser un número valido.");
+        alert.showAndWait();
+    }
+    private void mostrarError2() {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("ERROR");
+        alert.setHeaderText(null);
+        alert.setContentText("La cantidad debe ser un número valido.");
+        alert.showAndWait();
+    }
+
 
     @FXML
     void MouseClickMostrar(MouseEvent event) {
